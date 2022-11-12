a214e1531dea4db9862a1e3421f67fcd21ea19e8
muhzub-vagrant <112322082+muhzub-vagrant@users.noreply.github.com>
2022-11-12 13:43:26 +1300

    Update nodejs.groovy

--- nodejs.groovy
+++ nodejs.groovy
@@ -3,10 +3,10 @@ job('Z_NodeJs Example')
 //fetch code from z_docker-demo.git and give configuration name and email    
     scm {
         git('https://github.com/muhzub-vagrant/z_docker-demo.git') 
-//        { node -> 
-//        node / gitConfigName('muhzubvagrant')
-//        node / gitConfigEmail('muhzub-vagrant@gmail.com')
-//        }
+        { node -> 
+        node / gitConfigName('muhzubvagrant')
+        node / gitConfigEmail('muhzub-vagrant@gmail.com')
+        }
     }
 
 //trigger build after every 5 min
